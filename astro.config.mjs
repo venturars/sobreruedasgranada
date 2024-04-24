3fa0097c41093692f4ad833d053901b5aa26d21d
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-04-24 13:05:20 +0200

    add copies of footer, faqs and location

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   image: {
     service: passthroughImageService(),
   },
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
